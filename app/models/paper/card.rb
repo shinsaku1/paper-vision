15d907b214b0dc292adfb12a711f95d53ac93a04
CHIKURA Shinsaku <shinsaku@chikura.me>
2024-06-24 07:26:12 +0900

    Analyze URL

--- app/models/paper/card.rb
+++ app/models/paper/card.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Paper::Card < Paper
-  FIELDS = %i[ name reading company division title tel mobile email zip_code address qr ]
+  FIELDS = %i[ name reading company division title tel mobile email zip_code address url qr ]
   jsonb_accessor :attrs,
                  Hash[*FIELDS.map { |f| [f, :string] }.flatten]
 
